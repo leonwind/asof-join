d542d68e9a5360366648bfecacbd397b07639661
Leon Windheuser <leon.windheuser@gmail.com>
2024-10-22 17:16:04 +0200

    Add script to generate small orderbook

---
+++ scripts/generate_random_order_book.py
@@ -0,0 +1,39 @@
+import csv
+import random
+
+
+def generate_random_timestamp(earliest, latest):
+    return random.randint(earliest, latest)
+
+
+def generate_random_amount(max_amount):
+    return random.randint(0, max_amount)
+
+
+def get_random_stock_id(stock_ids):
+    return random.choice(stock_ids)
+
+
+def write_csv(num_orders, stock_ids, earliest_time, latest_time, max_amount, file_name):
+    with open(file_name, "w") as file:
+        writer = csv.writer(file)
+
+        for _ in range(num_orders):
+            timestamp = generate_random_timestamp(earliest_time, latest_time)
+            stock_id = get_random_stock_id(stock_ids)
+            amount = generate_random_amount(max_amount)
+            writer.writerow((timestamp, stock_id, amount))
+
+
+def generate_small_btc_orderbook():
+    write_csv(
+        num_orders=10,
+        stock_ids=["BTC"],
+        earliest_time=1325412060,
+        latest_time=1729122660,
+        max_amount=10,
+        file_name="../data/btc_orderbook_small.csv")
+
+
+if __name__ == "__main__":
+    generate_small_btc_orderbook()
--- scripts/transform_btc_data.py
+++ scripts/transform_btc_data.py
@@ -3,24 +3,24 @@ import csv
 
 def read_and_delete_csv(source_path, target_path):
     with open(source_path, "r") as source:
-        csv_reader = csv.reader(source)
+        reader = csv.reader(source)
  
         # Skip the header
-        next(csv_reader, None)  
+        next(reader, None)
 
         with open(target_path, "w") as target:
-            csv_writer = csv.writer(target)
+            writer = csv.writer(target)
 
-            for row in csv_reader:
+            for row in reader:
                 timestamp = int(float(row[0]))
                 btc_id = "BTC"
                 high_price_cent = int(float(row[2]) * 100)
                 low_price_cent = int(float(row[3]) * 100)
                 avg_price = int((high_price_cent + low_price_cent) / 2.0)
 
-                csv_writer.writerow((timestamp, btc_id, avg_price))
+                writer.writerow((timestamp, btc_id, avg_price))
 
 
 if __name__ == "__main__":
-    read_and_delete_csv("btcusd_1-min_data.csv", "btc_usd_data.csv")
+    read_and_delete_csv("../data/btcusd_1-min_data.csv", "../data/btc_usd_data.csv")
 
