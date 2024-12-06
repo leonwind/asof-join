64f367342377edf10f0ed49fe0917423a1dd9863
Leon Windheuser <leon.windheuser@gmail.com>
2024-12-06 15:23:13 +0100

    Add test for more keys than partitions

--- test/parallel_multi_map_test.cc
+++ test/parallel_multi_map_test.cc
@@ -61,3 +61,27 @@ TEST(multimap, SmallInsertLookup) {
         }
     }
 }
+
+TEST(multimap, MoreKeysThanPartitions) {
+    size_t num_keys = 4096;
+    size_t num_entries_per_key = 1000;
+    auto [keys, values] = generate_data(
+        num_keys,
+        num_entries_per_key);
+
+    MultiMap<TestEntry> multi_map(keys, values);
+
+    for (size_t i = 0; i < num_keys; ++i) {
+        auto key = generate_key(i);
+        auto& data = multi_map[key];
+        ASSERT_EQ(data.size(), num_entries_per_key);
+
+        std::sort(data.begin(), data.end());
+        for (size_t j = 0; j < num_entries_per_key; ++j) {
+            std::string error_msg = fmt::format("Failed at key {}, j={}", key, j);
+            size_t correct_value = /* offset= */ i + j * num_keys;
+            ASSERT_EQ(data[j].timestamp, correct_value) << error_msg;
+            ASSERT_EQ(data[j].idx, correct_value) << error_msg;
+        }
+    }
+}
