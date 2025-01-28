7ac9ddf863a3b0a25d5cd93902052715b0cfc59e
Leon Windheuser <leon.windheuser@gmail.com>
2025-01-28 21:02:08 +0100

    Add left partitioning + copy left partition for each thread

---
+++ src/algorithms/copy_left_partitioning_join.cc
@@ -0,0 +1,171 @@
+#include "asof_join.hpp"
+#include "timer.hpp"
+#include "log.hpp"
+#include "parallel_multi_map.hpp"
+#include "searches.hpp"
+#include <fmt/format.h>
+#include <unordered_map>
+#include <mutex>
+#include "tbb/parallel_sort.h"
+#include "tbb/parallel_for.h"
+#include "tbb/parallel_for_each.h"
+
+
+// Morsel size is 16384
+#define MORSEL_SIZE (2<<14)
+
+void PartitioningLeftCopyLeftASOFJoin::join() {
+    PerfEvent e;
+    Timer<milliseconds> timer;
+    timer.start();
+
+    //e.startCounters();
+    MultiMapTB<LeftEntryCopy> order_book_lookup(order_book.stock_ids, order_book.timestamps);
+    tbb::enumerable_thread_specific<MultiMapTB<LeftEntryCopy>> order_book_lookups(
+        [&]{ return order_book_lookup; });
+    //e.stopCounters();
+    log("Partitioning Perf");
+    //e.printReport(std::cout, order_book.size);
+    log(fmt::format("Partitioning in {}{}", timer.lap(), timer.unit()));
+
+    //e.startCounters();
+    tbb::parallel_for_each(order_book_lookup.begin(), order_book_lookup.end(),
+            [&](auto& iter) {
+        tbb::parallel_sort(iter.second.begin(), iter.second.end());
+    });
+    //e.stopCounters();
+    log("\n\nSorting Perf: ");
+    //e.printReport(std::cout, prices.size);
+    log(fmt::format("Sorting in {}{}", timer.lap(), timer.unit()));
+
+    e.startCounters();
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, prices.size, MORSEL_SIZE),
+            [&](tbb::blocked_range<size_t>& range) {
+        auto& local_order_book_lookup = order_book_lookups.local();
+
+        for (size_t i = range.begin(); i < range.end(); ++i) {
+            const auto& stock_id = prices.stock_ids[i];
+            if (!local_order_book_lookup.contains(stock_id)) {
+                continue;
+            }
+
+            auto& partition_bin = local_order_book_lookup[stock_id];
+            auto timestamp = prices.timestamps[i];
+            auto* match= Search::Interpolation::greater_equal_than(
+                /* data= */ partition_bin,
+                /* target= */ timestamp);
+
+            if (match != nullptr) {
+                uint64_t diff = match->timestamp - timestamp;
+                if (diff < match->diff) {
+                    match->diff = diff;
+                    match->price_idx = i;
+                    match->matched = true;
+                }
+            }
+        }
+    });
+    e.stopCounters();
+    log("\n\nBinary Search Perf:");
+    log(e.getReport(prices.size));
+    log(fmt::format("Binary Search in {}{}", timer.lap(), timer.unit()));
+
+    /// Merge all thread local order book lookups into the global [[order_book_lookup]].
+    tbb::parallel_for_each(order_book_lookup.begin(), order_book_lookup.end(),
+            [&](auto& s_id_bin) {
+        auto& s_id = s_id_bin.first;
+        auto& global_bin = s_id_bin.second;
+
+        tbb::parallel_for(tbb::blocked_range<size_t>(0, global_bin.size()),
+            [&](tbb::blocked_range<size_t>& range) {
+
+            tbb::parallel_for_each(order_book_lookups.begin(), order_book_lookups.end(),
+                [&](auto& local_order_book) {
+
+                auto& local_bin = local_order_book[s_id];
+                for (size_t i = range.begin(); i < range.end(); ++i) {
+                    if (local_bin[i].matched && local_bin[i].diff < global_bin[i].diff) {
+                        global_bin[i].diff = local_bin[i].diff;
+                        global_bin[i].price_idx = local_bin[i].price_idx;
+                        global_bin[i].matched = true;
+                    }
+                }
+            });
+        });
+    });
+
+
+    e.startCounters();
+    tbb::parallel_for_each(order_book_lookup.begin(), order_book_lookup.end(),
+            [&](auto& iter) {
+        std::vector<LeftEntryCopy>& partition_bin = iter.second;
+        const size_t num_thread_chunks = (partition_bin.size() + MORSEL_SIZE - 1) / MORSEL_SIZE;
+        std::vector<LeftEntryCopy*> last_match_per_range(num_thread_chunks);
+
+        /// We have to parallel iterate over the number of thread chunks since TBB is not forced to align
+        /// each chunk to [[MORSEL_SIZE]] which would make [[range.begin() / MORSEL_SIZE]] a non-correct
+        /// chunk position.
+        /// This would yield wrong results.
+        tbb::blocked_range<size_t> chunks_range(0, num_thread_chunks);
+
+        tbb::parallel_for(chunks_range,
+                [&](tbb::blocked_range<size_t>& chunks_range) {
+            for (size_t chunks_idx = chunks_range.begin(); chunks_idx != chunks_range.end(); ++chunks_idx) {
+                size_t start = chunks_idx * MORSEL_SIZE;
+                size_t end = std::min(chunks_idx * MORSEL_SIZE + MORSEL_SIZE,
+                                      partition_bin.size());
+                LeftEntryCopy *last_match = nullptr;
+
+                for (size_t i = end; i != start; --i) {
+                    if (partition_bin[i - 1].matched) {
+                        last_match = &partition_bin[i - 1];
+                        break;
+                    }
+                }
+                size_t morsel_pos = start / MORSEL_SIZE;
+                last_match_per_range[morsel_pos] = last_match;
+            }
+        });
+
+        tbb::parallel_for(chunks_range,
+                [&](tbb::blocked_range<size_t>& chunks_range) {
+            for (size_t chunks_idx = chunks_range.begin(); chunks_idx != chunks_range.end(); ++chunks_idx) {
+                size_t start = chunks_idx * MORSEL_SIZE;
+                size_t end = std::min(chunks_idx * MORSEL_SIZE + MORSEL_SIZE,
+                                      partition_bin.size());
+                LeftEntryCopy *last_match = nullptr;
+
+                size_t morsel_pos = start / MORSEL_SIZE;
+                for (size_t i = morsel_pos; i != 0; --i) {
+                    if (last_match_per_range[i - 1] != nullptr) {
+                        last_match = last_match_per_range[i - 1];
+                        break;
+                    }
+                }
+
+                for (size_t i = start; i != end; ++i) {
+                    auto &entry = partition_bin[i];
+                    if (entry.matched) {
+                        last_match = &entry;
+                    }
+
+                    if (last_match && last_match->matched) {
+                        result.insert(
+                                /* price_timestamp= */ prices.timestamps[last_match->price_idx],
+                                /* price_stock_id= */ prices.stock_ids[last_match->price_idx],
+                                /* price= */ prices.prices[last_match->price_idx],
+                                /* order_book_timestamp= */ order_book.timestamps[entry.order_idx],
+                                /* order_book_stock_id= */ order_book.stock_ids[entry.order_idx],
+                                /* amount= */ order_book.amounts[entry.order_idx]);
+                    }
+                }
+            }
+        });
+    });
+    e.stopCounters();
+    log("\n\nFinding Match Perf:");
+    log(e.getReport(order_book.size));
+    log(fmt::format("Finding match in {}{}", timer.lap(), timer.unit()));
+
+    result.finalize();
+}
--- test/asof_join_test.cc
+++ test/asof_join_test.cc
@@ -529,3 +529,65 @@ TEST(asof_join_partitioning_right_filter_min, TestSmallFilterinExample) {
     ASSERT_EQ(join.result.size, 6);
     ASSERT_EQ(join.result.value_sum, 3055);
 }
+
+TEST(asof_join_partitioning_left_copy, TestSmallDuckDBExample) {
+    Prices prices = load_prices("../test/data/prices_small.csv", ',', true);
+    OrderBook order_book = load_order_book("../test/data/orderbook_small.csv", ',', true);
+    PartitioningLeftCopyLeftASOFJoin join(prices, order_book, LESS_EQUAL_THAN, INNER);
+
+    join.join();
+
+    ASSERT_EQ(join.result.size, 4);
+    ASSERT_EQ(join.result.value_sum, 9581);
+}
+
+TEST(asof_join_partitioning_left_copy, TestSmallBTCExample) {
+    Prices prices = load_prices("../test/data/btc_usd_data.csv", ',', true);
+    OrderBook order_book = load_order_book("../test/data/btc_orderbook_small.csv", ',', true);
+    PartitioningLeftCopyLeftASOFJoin join(prices, order_book, LESS_EQUAL_THAN, INNER);
+
+    join.join();
+    auto result_values = join.result.collect_values();
+    std::sort(result_values.begin(), result_values.end());
+
+    std::vector<uint64_t> correct_values = {
+        5244, 9632, 247800, 523980, 4648032, 11277600, 11787330, 13727200, 33081768, 35807400
+    };
+    ASSERT_EQ(result_values.size(), correct_values.size());
+    for (size_t i = 0; i < result_values.size(); ++i) {
+        ASSERT_EQ(result_values[i], correct_values[i]) << "Failed at index " << i;
+    }
+}
+
+TEST(asof_join_partitioning_left_copy, TestMediumBTCExample) {
+    Prices prices = load_prices("../test/data/btc_usd_data.csv", ',', true);
+    OrderBook order_book = load_order_book("../test/data/btc_orderbook_medium.csv", ',', true);
+    PartitioningLeftCopyLeftASOFJoin join(prices, order_book, LESS_EQUAL_THAN, INNER);
+
+    join.join();
+
+    ASSERT_EQ(join.result.size, 10000);
+    ASSERT_EQ(join.result.value_sum, 70580346356);
+}
+
+TEST(asof_join_partitioning_left_copy, TestSmallUniformExample) {
+    Prices prices = load_prices("../test/data/uniform_small_prices.csv", ',', true);
+    OrderBook order_book = load_order_book("../test/data/uniform_small_positions.csv", ',', true);
+    PartitioningLeftCopyLeftASOFJoin join(prices, order_book, LESS_EQUAL_THAN, INNER);
+
+    join.join();
+
+    ASSERT_EQ(join.result.size, 10000);
+    ASSERT_EQ(join.result.value_sum, 24168000);
+}
+
+TEST(asof_join_partitioning_left_copy, TestSmallZipfExample) {
+    Prices prices = load_prices("../test/data/zipf_small_prices.csv", ',', true);
+    OrderBook order_book = load_order_book("../test/data/zipf_small_positions.csv", ',', true);
+    PartitioningLeftCopyLeftASOFJoin join(prices, order_book, LESS_EQUAL_THAN, INNER);
+
+    join.join();
+
+    ASSERT_EQ(join.result.size, 1024);
+    ASSERT_EQ(join.result.value_sum, 2532396);
+}
